313f063e7ba0eb1bf56459045b8b617df57fcf50
BitLee-design <eliferbit@gmail.com>
2022-06-21 16:47:19 +0000

    second commit

---
+++ bitposts/frontend/src/components/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { ReactDOM } from "react-dom";
+
+class App extends Comment {
+    render() {
+        return <h1>New app</h1>
+    }
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
+
